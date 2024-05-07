26a9c564cb9c57c104c25383d57a84af9841fa13
Dafa24 <53173575+Dafa24@users.noreply.github.com>
2024-05-07 12:59:17 +0700

    Inisialisasi Projek dan Adding API with Model Data and Controller

---
+++ API_Barang/Controllers/BarangController.cs
@@ -0,0 +1,64 @@
+﻿using API_Barang;
+using API_Barang.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API_TUBES_KPL_KELOMPOK_05.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BukuController : Controller
+    {
+        private static List<Barang> DataDisplayBarang = new List<Barang>();
+
+        public BukuController()
+        {
+            string jsonFilePath = "D:\\TugasBesar_KPL\\API_Barang\\Data\\DataBarang.json";
+            string jsonData = System.IO.File.ReadAllText(jsonFilePath);
+            DataDisplayBarang = JsonConvert.DeserializeObject<List<Barang>>(jsonData);
+        }
+
+        public static List<Barang> getDataDisplayBook()
+        {
+            return DataDisplayBarang;
+        }
+
+        // GET DATA ALL API
+        [HttpGet]
+        public IEnumerable<Barang> GET()
+        {
+            return DataDisplayBarang;
+        }
+
+        // GET BOOK BY NamaBarang
+        [HttpGet("{NamaBarang}")]
+        public ActionResult<Barang> GET(string NamaBarang)
+        {
+            try
+            {
+                int id = -1;
+
+                for (int i = 0; i < DataDisplayBarang.Count; i++)
+                {
+                    if (NamaBarang == DataDisplayBarang[i].NamaBarang)
+                    {
+                        id = i;
+                    }
+                }
+
+                if (id != -1)
+                {
+                    return DataDisplayBarang[id];
+                }
+                else
+                {
+                    return NotFound("Nama Barang tidak ditemukan!"); // Mengembalikan respons 404 Not Found dengan pesan
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Terjadi kesalahan: " + ex.Message); // Mengembalikan respons 500 Internal Server Error dengan pesan
+            }
+        }
+    }
+}
---
+++ API_Barang/Model/Barang.cs
@@ -0,0 +1,23 @@
+﻿namespace API_Barang.Model
+{
+    public class Barang
+    {
+        public string NamaBarang { get; set; }
+
+        public int KodeBarang { get; set; }
+
+        public int JumlahBarang { get; set; }
+
+        public int HargaBarang { get; set; }
+
+        public Barang(string NamaBarang, int KodeBarang, int JumlahBarang, int HargaBarang)
+        {
+            this.NamaBarang = NamaBarang;
+            this.KodeBarang = KodeBarang;
+            this.JumlahBarang = JumlahBarang;
+            this.HargaBarang = HargaBarang;
+        }
+
+    }
+}
+
